694db4a319563b6be332a23bd29e95f48c683940
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-08-06 19:02:17 +0530

    feat: add addNewLaunch function

--- server/src/models/launches/index.js
+++ server/src/models/launches/index.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 1;
+
 const launch = {
   flightNumber: 1,
   mission: "Mark 1",
@@ -17,6 +19,20 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber += 1;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      flightNumber: latestFlightNumber,
+      customer: ["NASA"],
+      upcoming: true,
+      success: true
+    })
+  );
+}
+
 module.exports = {
-  getAllLaunches
+  getAllLaunches,
+  addNewLaunch
 };
